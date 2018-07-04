29af38fb07966792228484b5e58afe1aba6770be
pwujczyk <pwujczyk@ecovadis.com>
2018-07-04 13:57:25 +0200

    change name of the project

--- MasterConfiguration/Builders/File.cs
+++ MasterConfiguration/Builders/File.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace MasterConfiguration.Builders
+namespace ProductivityTools.MasterConfiguration.Builders
 {
     class File
     {
