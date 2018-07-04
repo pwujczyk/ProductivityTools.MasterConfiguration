8bb19542be2e7ab59e3abae862ff8cca909e55ca
Pawel Wujczyk <pwujczyk@hotmail.com>
2018-07-04 13:57:25 +0200

    change name of the project

--- MasterConfiguration/ConfigurationDirector.cs
+++ MasterConfiguration/ConfigurationDirector.cs
@@ -1,4 +1,4 @@
-﻿using MasterConfiguration.Builders;
+﻿using ProductivityTools.MasterConfiguration.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace MasterConfiguration
+namespace ProductivityTools.MasterConfiguration
 {
     class ConfigurationDirector
     {
