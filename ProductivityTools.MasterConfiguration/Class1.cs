ddfcf91d5d420111c52e3e5fc7b2c8cbfb47de49
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-06-22 14:16:25 +0200

    new project

---
+++ ProductivityTools.MasterConfiguration/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ProductivityTools.MasterConfiguration
+{
+    public static class Class1
+    {
+        public static IConfigurationBuilder AddMasterConfiguration(this IConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.AddJsonFile(@"d:\ProductivityTools.Bank.Millenium.json");
+            return configurationBuilder;
+        }
+    }
+}
