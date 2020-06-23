6c8f883b8668fc9abc8b995c70c2eb629ddc6f74
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-06-23 23:20:04 +0200

    force paramter

--- ProductivityTools.MasterConfiguration/Class1.cs
+++ ProductivityTools.MasterConfiguration/Class1.cs
@@ -1,14 +1,39 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
+using System.Reflection;
 
 namespace ProductivityTools.MasterConfiguration
 {
     public static class Class1
     {
-        public static IConfigurationBuilder AddMasterConfiguration(this IConfigurationBuilder configurationBuilder)
+        private static IConfigurationBuilder AddMasterConfigurationInternal(this IConfigurationBuilder configurationBuilder, string configName, bool force)
         {
-            configurationBuilder.AddJsonFile(@"d:\ProductivityTools.Bank.Millenium.json");
+            string aspnetCoreEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (force || aspnetCoreEnvironment == "MasterConfiguration")
+            {
+                string path = Environment.GetEnvironmentVariable("MasterConfigurationPath");
+                if (string.IsNullOrEmpty(path))
+                {
+                    throw new Exception("Application uses MasterConfiguration, but MasterConfigurationPath hadn't been setup. Please setup MasterConfigurationPath environment variable which will point to the directory where master configuration is stored");
+                }
+                configurationBuilder.AddJsonFile(configName);
+            }
             return configurationBuilder;
         }
+
+        public static IConfigurationBuilder AddMasterConfiguration(this IConfigurationBuilder configurationBuilder, string configName, bool force)
+        {
+            var r = configurationBuilder.AddMasterConfigurationInternal(configName, force);
+            return r;
+        }
+
+        public static IConfigurationBuilder AddMasterConfiguration(this IConfigurationBuilder configurationBuilder, bool force)
+        {
+            var x = Assembly.GetCallingAssembly();
+            var configName = x.GetName().Name;
+            var r = configurationBuilder.AddMasterConfigurationInternal($"{configName}.json", force);
+            return r;
+        }
+
     }
 }
